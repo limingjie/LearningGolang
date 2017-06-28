dc03b90402499bade474a16d1add10159af130fb
Mingjie Li <limingjie@outlook.com>
2017-06-29 00:02:41 +0800

    sync by unbuffered channel

---
+++ src/gopl/ch8/ex83/netcat3.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(nil)
+	}
+	done := make(chan bool)
+	go func() {
+		io.Copy(os.Stdout, conn) // Note: ignoring errors
+		log.Println("done")
+		done <- true
+	}()
+	mustCopy(conn, os.Stdin)
+	conn.Close()
+	<-done
+}
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Fatal(err)
+	}
+}
